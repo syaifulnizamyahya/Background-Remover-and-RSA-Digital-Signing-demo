92c752af8ff0e140b44becfce346ff24fffad290
syaifulnizamyahya <syaifulnizamyahya@gmail.com>
2024-07-29 13:14:45 +0800

    Working Background removal UI

--- WpfTemplateStudio/ViewModels/BackgroundRemoverViewModel.cs
+++ WpfTemplateStudio/ViewModels/BackgroundRemoverViewModel.cs
@@ -1,11 +1,93 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using Microsoft.Win32;
+using System.Collections;
+using System.IO;
+using System.Security.Cryptography.X509Certificates;
+using System.Windows.Media.Imaging;
+using WpfTemplateStudio.Core.Services;
 
 namespace WpfTemplateStudio.ViewModels;
 
-public class BackgroundRemoverViewModel : ObservableObject
+public partial class BackgroundRemoverViewModel : ObservableObject
 {
     public BackgroundRemoverViewModel()
     {
     }
+
+    [ObservableProperty]
+    private string statusMessage;
+
+    [ObservableProperty]
+    private BitmapImage selectedImage;
+
+    [ObservableProperty]
+    private BitmapImage removedBackgroundImage1;
+
+    [ObservableProperty]
+    private BitmapImage removedBackgroundImage2;
+
+    [ObservableProperty]
+    private BitmapImage removedBackgroundImage3;
+
+    [ObservableProperty]
+    private BitmapImage removedBackgroundImage4;
+
+    [ObservableProperty]
+    private BitmapImage removedBackgroundImage5;
+
+    [ObservableProperty]
+    private string filePath;
+
+    [RelayCommand]
+    private async Task SelectImageAsync()
+    {
+        StatusMessage = "Selecting image...";
+
+        await Task.Run(() =>
+        {
+            var openFileDialog = new OpenFileDialog
+            {
+                Filter = "Image files (*.jpg, *.jpeg, *.png)|*.jpg;*.jpeg;*.png"
+            };
+
+            bool? result = openFileDialog.ShowDialog();
+
+            if (result == true)
+            {
+                FilePath = openFileDialog.FileName;
+                BitmapImage bitmap = new BitmapImage();
+                bitmap.BeginInit();
+                bitmap.UriSource = new Uri(FilePath);
+                bitmap.EndInit();
+                bitmap.Freeze(); 
+                SelectedImage = bitmap;
+            }
+        });
+
+        StatusMessage = "Image selected";
+    }
+
+    [RelayCommand]
+    private async Task RemoveBackgroundAsync()
+    {
+        await Task.Run(() =>
+        {
+            var result = BackgroundRemoverService.RemoveBackground(FilePath);
+
+            if (result != null)
+            {
+                using (MemoryStream memoryStream = new MemoryStream(result))
+                {
+                    RemovedBackgroundImage1 = new BitmapImage();
+                    RemovedBackgroundImage1.BeginInit();
+                    RemovedBackgroundImage1.CacheOption = BitmapCacheOption.OnLoad;
+                    RemovedBackgroundImage1.StreamSource = memoryStream;
+                    RemovedBackgroundImage1.EndInit();
+                }
+                RemovedBackgroundImage1.Freeze();
+            }
+        });
+    }
 }
+
